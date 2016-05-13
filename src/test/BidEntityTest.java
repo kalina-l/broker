c6e1006712e8b21087d4cc02a0b147cbebbe239c
kalina <kalina.lebecka@gmail.com>
2016-05-13 18:50:26 +0200

    BidEntity - testConstraints PersonEntity - testConstraints (zum Teil)

--- src/test/BidEntityTest.java
+++ src/test/BidEntityTest.java
@@ -1,7 +1,39 @@
 package test;
+import model.*;
+import org.junit.*;
+import javax.persistence.*;
+import javax.validation.Validator;
 
 public class BidEntityTest extends EntityTest{
 
+	@Test
+	public void testConstraints(){
+		Validator v = this.getEntityValidatorFactory().getValidator();
+		Person seller = new Person();
+		Person bidder = new Person();
+		Auction auction = new Auction(seller);
+		Bid bid = new Bid(auction, bidder);
+		
+		// PRICE
+		bid.setPrice(0);
+		Assert.assertEquals(0, v.validate(bid).size());
+		bid.setPrice(-1);
+		Assert.assertEquals(1, v.validate(bid).size());
+	}
 	
-	
+	@Test
+	public void testLifeCycle(){
+		EntityManagerFactory  emf = super.getEntityManagerFactory();
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		Person seller = new Person();
+		Person bidder = new Person();
+		Auction auction = new Auction(seller);
+		Bid bid = new Bid(auction, bidder);
+		em.persist(bid);
+		em.getTransaction().commit();
+		
+		em.close();
+	}
 }
--- src/test/PersonEntityTest.java
+++ src/test/PersonEntityTest.java
@@ -1,5 +1,57 @@
 package test;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.validation.Validator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import model.Auction;
+import model.Bid;
+import model.Person;
+
 public class PersonEntityTest extends EntityTest{
 
+	@Test
+	public void testConstraints(){
+		Validator v = this.getEntityValidatorFactory().getValidator();
+		Person person = new Person();
+		
+		// password hash
+		person.setPasswordHash(new byte[32]);
+		Assert.assertEquals(0, v.validate(person).size());
+		person.setPasswordHash(new byte[31]);
+		Assert.assertEquals(1, v.validate(person).size());
+		
+		// name - family
+		person.getName().setFamily("Maier");
+		Assert.assertEquals(0, v.validate(person).size());
+		person.getName().setFamily("");
+		Assert.assertEquals(1, v.validate(person).size());
+		person.getName().setFamily("Maierowskimaierowskimaierowskimaierowski");
+		Assert.assertEquals(1, v.validate(person).size());
+		
+		// name - given
+		person.getName().setGiven("Annika");
+		Assert.assertEquals(0, v.validate(person).size());
+		person.getName().setGiven("");
+		Assert.assertEquals(1, v.validate(person).size());
+		person.getName().setGiven("Annikaannikaannikaannikaannikaannika");
+		Assert.assertEquals(1, v.validate(person).size());
+		
+		// address - street
+		person.getAddress().setStreet("Warschauerstr.");
+		Assert.assertEquals(0, v.validate(person).size());
+		
+		// address - postCode
+		person.getAddress().setPostalCode("10429");
+		Assert.assertEquals(0, v.validate(person).size());
+	}
+	
+	@Test
+	public void testLifeCycle(){
+		
+	}
+	
 }
