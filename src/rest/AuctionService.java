1938a2194ac0c58c73c723f35e7fa38385bc14c4
DAiRed <hady@khalifa.eu>
2016-06-11 16:38:57 +0200

    GET /services/people/id/auction, GET /services/people/id/bids, GET /services/auctions and GET /services/auctions in PersonService and AuctionService works fine. HOTFIX - ServiceTest need rest package as configRessource

--- src/rest/AuctionService.java
+++ src/rest/AuctionService.java
@@ -1,15 +1,97 @@
 package rest;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityNotFoundException;
 import javax.persistence.Persistence;
+import javax.persistence.RollbackException;
+import javax.persistence.TypedQuery;
 import javax.ws.rs.*;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.Response;
+
+import jersey.repackaged.com.google.common.collect.Lists;
+
+import static javax.ws.rs.core.Response.Status.CONFLICT;
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
 
 import java.awt.PageAttributes.MediaType;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import model.*;
 
+@Path("/auctions")
 public class AuctionService {
 
+	static private EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("broker");
+	
+	@GET
+	@Path("/")
+	@Produces({"application/xml", "application/json"})
+	public Response getAuctions(){
+		EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+		TypedQuery<Auction> query;			
+		em.getTransaction().begin();
+		try{
+			try{
+				//Limit Paramter
+				String queryArg0 = "identity";
+				long lowerLimit0 = 1;
+				long upperLimit0 = 100;
+				
+				//Query limited range
+				//TODO - Named Queries Annotation in model?
+				query  = em.createQuery("SELECT x FROM Auction x WHERE "
+						+ "(" + lowerLimit0 + " is null or x." + queryArg0 + " >= " + lowerLimit0 + ") and"
+						+ "(" + upperLimit0 + " is null or x." + queryArg0 + " <= " + upperLimit0 + ")", 
+						Auction.class);
+			}
+			finally{
+				if (em.getTransaction().isActive()) {
+						em.getTransaction().rollback();
+				}	
+			}				
+			
+			//See - http://stackoverflow.com/questions/6081546/jersey-can-produce-listt-but-cannot-response-oklistt-build
+			List<Auction> list = query.getResultList();
+			GenericEntity<List<Auction>> entity = 
+		            new GenericEntity<List<Auction>>(Lists.newArrayList(list)) {};
+		        return Response.ok(entity).build();
+			
+		// TODO Errorhandling
+		}catch (final EntityNotFoundException exception) {
+			throw new ClientErrorException(NOT_FOUND);
+		} catch (final RollbackException exception) {
+			throw new ClientErrorException(CONFLICT);
+		} 
+	}
+	
+	
+	@GET
+	@Path("/{identity}")
+	@Produces({"application/xml", "application/json"})
+	public Response getAuctionByID(@PathParam("identity") long identity){
+		try{
+			EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
+			Auction auction;
+			em.getTransaction().begin();			
+			auction = em.find(Auction.class, identity);
+			try{
+					em.getTransaction().commit();
+			}finally{
+					if (em.getTransaction().isActive()) {
+						em.getTransaction().rollback();
+					}	
+				}
+			return Response.ok(auction).build();
+		
+		}catch (final EntityNotFoundException exception) {
+			throw new ClientErrorException(NOT_FOUND);
+		} catch (final RollbackException exception) {
+			throw new ClientErrorException(CONFLICT);
+		}
+	}
+	
+	
 }
--- src/rest/PersonService.java
+++ src/rest/PersonService.java
@@ -3,9 +3,11 @@ package rest;
 import static javax.ws.rs.core.Response.Status.CONFLICT;
 import static javax.ws.rs.core.Response.Status.FORBIDDEN;
 import static javax.ws.rs.core.Response.Status.NOT_FOUND;
-import static model.Group.*;
+import static model.Group.ADMIN;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -14,8 +16,10 @@ import javax.persistence.Persistence;
 import javax.persistence.RollbackException;
 import javax.persistence.TypedQuery;
 import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -24,6 +28,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import jersey.repackaged.com.google.common.collect.Lists;
+import model.Auction;
+import model.Bid;
 import model.Person;
 
 @Path("/people")
@@ -34,25 +40,36 @@ public class PersonService{
 	@GET
 	@Path("/")
 	@Produces({"application/xml", "application/json"})
-	public Response getPerson(){
+	public Response getPersons(){
 		EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
 		TypedQuery<Person> query;			
 		em.getTransaction().begin();
 		try{
 			try{
-				query  = em.createQuery("SELECT p FROM Person p", Person.class);
+				//Limit Paramter
+				int lowerNumber = 1;
+				int upperNumber = 100;
+				String queryAttr = "identity";
+				//Query limited range
+				//TODO - Named Queries Annotation in model?
+				query  = em.createQuery("SELECT p FROM Person p WHERE "
+						+ "(" + lowerNumber + " is null or p." + queryAttr + " >= " + lowerNumber + ") and"
+						+ "(" + upperNumber + " is null or p." + queryAttr + " <= " + upperNumber + ")", 
+						Person.class);
 			}
 			finally{
 				if (em.getTransaction().isActive()) {
 						em.getTransaction().rollback();
 				}	
 			}			
-			// See - http://stackoverflow.com/questions/6081546/jersey-can-produce-listt-but-cannot-response-oklistt-build
+			
+			//See - http://stackoverflow.com/questions/6081546/jersey-can-produce-listt-but-cannot-response-oklistt-build
 			List<Person> list = query.getResultList();
 			GenericEntity<List<Person>> entity = 
 		            new GenericEntity<List<Person>>(Lists.newArrayList(list)) {};
 		        return Response.ok(entity).build();
 			
+		// TODO Errorhandling
 		}catch (final EntityNotFoundException exception) {
 			throw new ClientErrorException(NOT_FOUND);
 		} catch (final RollbackException exception) {
@@ -70,13 +87,13 @@ public class PersonService{
 			EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();
 			Person person;
 			em.getTransaction().begin();			
+			person = em.find(Person.class, identity);
 			try{
-				person = em.find(Person.class, identity);
-			}
-			finally{
-				if (em.getTransaction().isActive()) {
-					em.getTransaction().rollback();
-				}	
+					em.getTransaction().commit();
+			}finally{
+					if (em.getTransaction().isActive()) {
+						em.getTransaction().rollback();
+					}	
 			}
 			return Response.ok(person).build();
 		
@@ -87,6 +104,133 @@ public class PersonService{
 		}
 	}
 	
+	
+	
+	@GET
+	@Path("/{identity}/auctions")
+	@Produces({"application/xml", "application/json"})
+	public Response getAuctionsByPersonID(@PathParam("identity") long identity){
+		
+		try{
+			EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();	
+			em.getTransaction().begin();
+			
+			Person person = em.find(Person.class, identity);
+			Set<Auction> auctions = person.getAuctions();
+			Set<Bid> bids = person.getBids();
+			// Get auctions with bidder reference
+			for(Bid b : bids){
+				//Notwendig?
+				if (b.getBidder() != null && b.getAuction() != null && b.getBidder().getIdentity() == person.getIdentity())
+					auctions.add(b.getAuction());
+			}
+			
+			
+			try{ // Start Commit --------------------
+				em.getTransaction().commit();
+			}finally{
+				if (em.getTransaction().isActive()) {
+					em.getTransaction().rollback();
+				}	
+			} // End Commit -------------------------
+
+			GenericEntity<List<Auction>> entity = 
+		            new GenericEntity<List<Auction>>(Lists.newArrayList(auctions)) {};
+		        return Response.ok(entity).build();
+			
+		// TODO Errorhandling
+		}catch (final EntityNotFoundException exception) {
+			throw new ClientErrorException(NOT_FOUND);
+		} catch (final RollbackException exception) {
+			throw new ClientErrorException(CONFLICT);
+		} 
+	}
+
+	
+	
+	@GET
+	@Path("/{identity}/bids")
+	@Produces({"application/xml", "application/json"})
+	public Response getClosedBidsByPersonID(@PathParam("identity") long identity){
+		
+		try{
+			EntityManager em = ENTITY_MANAGER_FACTORY.createEntityManager();	
+			em.getTransaction().begin();
+			
+			Person person = em.find(Person.class, identity);
+			Set<Bid> closedBids = new HashSet<Bid>();
+			Set<Bid> bids = person.getBids();
+			for(Bid b : bids){
+				if (b.getAuction().isClosed())
+					closedBids.add(b);
+			}
+				
+			try{ // Start Commit --------------------
+				em.getTransaction().commit();
+			}finally{
+				if (em.getTransaction().isActive()) {
+					em.getTransaction().rollback();
+				}	
+			} // End Commit -------------------------
+
+			GenericEntity<List<Bid>> entity = 
+		            new GenericEntity<List<Bid>>(Lists.newArrayList(closedBids)) {};
+		        return Response.ok(entity).build();
+			
+		// TODO Errorhandling
+		}catch (final EntityNotFoundException exception) {
+			throw new ClientErrorException(NOT_FOUND);
+		} catch (final RollbackException exception) {
+			throw new ClientErrorException(CONFLICT);
+		} 
+	}
+			
+			
+			
+			
+	
+	
+   @PUT
+   @Path("/")
+   @Produces({"application/xml", "application/json"})
+   @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+   public void createPerson(@HeaderParam("Authorization") final String authentication){
+	   EntityManager em = LifeCycleProvider.brokerManager();
+		Person requester = LifeCycleProvider.authenticate(authentication);
+		if (requester.getGroup() != ADMIN) throw new ClientErrorException(FORBIDDEN);
+			
+		Person person;
+			   try{
+			em.getTransaction().begin();	
+			person =  new Person();
+			
+			person.setAlias(randomIdentifier());
+			person.getName().setGiven(randomIdentifier());
+			person.getName().setFamily(randomIdentifier());
+			person.getAddress().setCity("Berlin-"+randomIdentifier());
+			person.getAddress().setStreet("Testallee 13");
+			person.getAddress().setPostalCode("12345");
+			person.getContact().setEmail(randomIdentifier()+"@test.com");
+			person.getContact().setPhone("012345678");
+
+			em.persist(person);
+				
+			try {
+				em.getTransaction().commit();
+			} finally {
+				em.getTransaction().begin();
+			}
+	
+			em.close();
+		}catch (final EntityNotFoundException exception) {
+			throw new ClientErrorException(NOT_FOUND);
+		} catch (final RollbackException exception) {
+			throw new ClientErrorException(CONFLICT);
+		}
+	}
+	   
+	  
+    
 	
 //	@PUT
 //	@Path("/people")
@@ -121,5 +265,29 @@ public class PersonService{
 //	 
 //	
 //	}
+
+   /*
+    * RANDOM NAME FUNCTION
+    * - todo seperate
+    */
+   
+//class variable
+final String lexicon = "ABCDEFGHIJKLMNOPQRSTUVWXYZ12345674890";
+final java.util.Random rand = new java.util.Random();
+//consider using a Map<String,Boolean> to say whether the identifier is being used or not 
+final Set<String> identifiers = new HashSet<String>();
+
+public String randomIdentifier() {
+ StringBuilder builder = new StringBuilder();
+ while(builder.toString().length() == 0) {
+     int length = rand.nextInt(5)+5;
+     for(int i = 0; i < length; i++)
+         builder.append(lexicon.charAt(rand.nextInt(lexicon.length())));
+     if(identifiers.contains(builder.toString()))
+         builder = new StringBuilder();
+ }
+ return builder.toString();
+}
+
 
 }
