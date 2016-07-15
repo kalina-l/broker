653fae0d1afdccf40528533faa042b77731869f1
DAiRed <hady@khalifa.eu>
2016-07-15 17:59:57 +0200

    test classes added

---
+++ src/test/AuctionServiceTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.junit.Test;
+
+public class AuctionServiceTest extends ServiceTest {
+
+	
+	@Test
+	public void testCriteriaQueries(){
+	 //TODO
+	
+	}
+	
+	@Test
+	public void testIdentityQueries(){
+	//TODO
+	
+	}
+	
+	@Test
+	public void testBidRelations(){
+	//TODO
+	
+	}
+	
+}
---
+++ src/test/PersonServiceTest.java
@@ -0,0 +1,41 @@
+package test;
+
+import org.junit.Test;
+
+public class PersonServiceTest extends ServiceTest{
+
+	
+
+	@Test
+	public void testCriteriaQueries(){
+	 //TODO
+	
+	}
+	
+	@Test
+	public void testIdentityQueries(){
+	//TODO
+	
+	}
+	
+	@Test
+	public void testBidRelations(){
+	//TODO
+	
+	}
+	
+	@Test
+	public void testAuctionRelationQueries(){
+	//TODO
+	
+	}
+	
+	
+	@Test
+	public void testLifeCycle(){
+	//TODO
+	
+	}
+	
+	
+}
